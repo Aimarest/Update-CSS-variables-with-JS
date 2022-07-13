bf8578844cf9a0cc5ec6ee967f9cfd3fdaf5310d
Ana <amarcosestevez@gmail.com>
2022-07-13 14:03:28 +0200

    add events and handle functions to change padding, blur and color at image

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 'use strict';
+
+//Variables de los elementos del DOM:
+
+const color = document.querySelector('.variables__color');
+const image = document.querySelector('.effects__image');
+const blurInput = document.querySelector('.variables__blur');
+const spacing = document.querySelector('.variables__spacing')
+//Funciones:
+function handleChangeBlur(ev){
+    console.log('Se está cambiando la visión',ev.target.value);
+    image.style.filter = `blur(${ev.target.value}px)`;
+}
+function handleChangeColor(ev){
+console.log('Está cambiando el color');
+console.log(ev.target.value)
+image.style.backgroundColor = ev.target.value;
+
+};
+
+function handleChangeSpacing(ev){
+    console.log('Se esta cambiando el padding de la imagen',ev.target.value,'px')
+image.style.padding = `${ev.target.value}px`;
+}
+blurInput.addEventListener('input',handleChangeBlur);
+color.addEventListener('input', handleChangeColor);
+spacing.addEventListener('input', handleChangeSpacing)
